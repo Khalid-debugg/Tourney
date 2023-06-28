cd99d7b5fc182c81686933752440ac787f07d22d
khalid-debugg <khaled20191700225@cis.asu.edu.eg>
2023-06-28 12:56:21 +0300

    logic done

--- JS/bracketsApp.js
+++ JS/bracketsApp.js
@@ -3,28 +3,31 @@ const run = () => {
   document.querySelector(
     "h1"
   ).textContent = `Welcome to "${tourTitle}" tournament!!`;
-  const brackets = [];
-  buildBrackets(brackets);
-  drawBrackets(brackets);
+  const tournament = [];
+  buildBrackets(tournament);
+  drawBrackets(tournament);
+  elemenateByes(tournament);
 };
-const updateBrackets = (brackets, btn) => {
+const updateBrackets = (tournament, btn) => {
   let roundId = btn.getAttribute("data-roundIndex");
   let bracketId = btn.getAttribute("data-bracketIndex");
   let playerId = parseInt(btn.getAttribute("data-playerIndex"));
-  const player = brackets[roundId][bracketId][playerId];
-  let indexInRound = bracketId * 2 + playerId;
-  let newindexInRound = Math.floor(indexInRound / 2);
-  bracketId = Math.floor(newindexInRound / 2);
-  playerId = newindexInRound % 2;
-  roundId++;
-  brackets[roundId][bracketId][playerId] = player;
-  const main = document.querySelector("main");
-  while (main.firstChild) {
-    main.removeChild(main.firstChild);
+  const player = tournament[roundId][bracketId][playerId];
+  const opponent = tournament[roundId][bracketId][(playerId + 1) % 2];
+  if (opponent.name !== 'TBD') {
+    player.setStatus('Winner');
+    opponent.setStatus('loser');
+    let indexInRound = bracketId * 2 + playerId;
+    let newindexInRound = Math.floor(indexInRound / 2);
+    bracketId = Math.floor(newindexInRound / 2);
+    playerId = newindexInRound % 2;
+    roundId++;
+    tournament[roundId][bracketId][playerId].name = player.name;
+    deleteBrackets();
+    drawBrackets(tournament);
   }
-  drawBrackets(brackets);
 };
-const buildBrackets = (brackets) => {
+const buildBrackets = (tournament) => {
   let participantsNo = localStorage.getItem("participantsNo");
   const participants = JSON.parse(localStorage.getItem("participants"));
   const numberOfRounds = Math.ceil(Math.log2(participantsNo));
@@ -32,25 +35,27 @@ const buildBrackets = (brackets) => {
   const byes = playersPerRound - participantsNo;
   shufflePlayers(participants);
   insertByes(participants, byes);
-  for (let i = 1; i <= numberOfRounds; i++) {
+  for (let i = 0; i < numberOfRounds; i++) {
     let round = [];
     for (let j = 0; j < playersPerRound; j += 2) {
       let bracket = [];
-      if (i === 1) {
-        bracket.push(participants[j], participants[j + 1]);
+      if (i === 0) {
+        bracket.push(new Player(participants[j], i, j / 2, 0, 'Idle'));
+        bracket.push(new Player(participants[j + 1], i, j / 2, 1, 'Idle'));
       } else {
-        bracket.push("TBD", "TBD");
+        bracket.push(new Player('TBD', i, j / 2, 0, 'Idle'));
+        bracket.push(new Player('TBD', i, j / 2, 1, 'Idle'));
       }
       round.push(bracket);
     }
-    brackets.push(round);
+    tournament.push(round);
     playersPerRound /= 2;
   }
-  console.log(brackets);
+  console.log(tournament);
   console.log(participants);
 };
-const drawBrackets = (brackets) => {
-  for (let i = 0; i < brackets.length; i++) {
+const drawBrackets = (tournament) => {
+  for (let i = 0; i < tournament.length; i++) {
     let round = document.createElement("div");
     round.classList.add(
       "flex",
@@ -62,21 +67,23 @@ const drawBrackets = (brackets) => {
     let roundNum = document.createElement("h2");
     roundNum.textContent = `Round ${i + 1}`;
     round.appendChild(roundNum);
-    for (let j = 0; j < brackets[i].length; j++) {
-      let bracket = new Bracket(
-        brackets[i][j][0],
-        i,
-        j,
-        0,
-        brackets[i][j][1],
-        i,
-        j,
-        1
+    for (let j = 0; j < tournament[i].length; j++) {
+      let bracket = document.createElement("div");
+      bracket.classList.add(
+        "flex",
+        "flex-col",
+        "p3",
+        "w-full"
       );
       const parser = new DOMParser();
-      let div = parser.parseFromString(bracket.generateBracket(), "text/html");
-      div = div.getElementsByTagName("div")[0];
-      round.appendChild(div);
+      let btn = parser.parseFromString(tournament[i][j][0].getBtn(), 'text/html');
+      btn = btn.querySelector('button');
+      bracket.appendChild(btn);
+      btn = parser.parseFromString(tournament[i][j][1].getBtn(), 'text/html');
+      btn = btn.querySelector('button');
+      bracket.appendChild(btn);
+
+      round.appendChild(bracket);
     }
     document.querySelector("main").appendChild(round);
   }
@@ -84,7 +91,7 @@ const drawBrackets = (brackets) => {
   buttons.forEach((btn) => {
     btn.addEventListener("click", (event) => {
       let btn = event.target;
-      updateBrackets(brackets, btn);
+      if (btn.textContent !== 'TBD' && btn.textContent !== 'bye') updateBrackets(tournament, btn);
     });
   });
 };
@@ -111,31 +118,44 @@ const insertByes = (array, byes) => {
     counter += 2;
   }
 };
-
-class Bracket {
-  constructor(
-    player1,
-    roundIndex1,
-    bracketIndex1,
-    playerIndex1,
-    player2,
-    roundIndex2,
-    bracketIndex2,
-    playerIndex2
-  ) {
-    this.player1 = player1;
-    this.player2 = player2;
-    this.roundIndex1 = roundIndex1;
-    this.bracketIndex1 = bracketIndex1;
-    this.playerIndex1 = playerIndex1;
-    this.roundIndex2 = roundIndex2;
-    this.bracketIndex2 = bracketIndex2;
-    this.playerIndex2 = playerIndex2;
+const elemenateByes = (tournament) => {
+  for (let i = 0; i < tournament[0].length; i++) {
+    if (tournament[0][i][1].name === 'bye') {
+      let indexInRound = i * 2 + 0;
+      let newindexInRound = Math.floor(indexInRound / 2);
+      tournament[1][Math.floor(newindexInRound / 2)][newindexInRound % 2].name = tournament[0][i][0].name;
+      tournament[0][i][0].setStatus('Winner');
+      tournament[0][i][1].setStatus('Loser');
+    }
   }
-  generateBracket() {
-    return `<div class="flex flex-col bg-slate-500 p3 w-full">
-                    <button class="p-3" data-roundIndex=${this.roundIndex1}  data-bracketIndex=${this.bracketIndex1}  data-playerIndex=${this.playerIndex1}>${this.player1}</button>
-                    <button class="p-3" data-roundIndex=${this.roundIndex2}  data-bracketIndex=${this.bracketIndex2}  data-playerIndex=${this.playerIndex2}>${this.player2}</button>
-            </div>`;
+  deleteBrackets();
+  drawBrackets(tournament);
+}
+const deleteBrackets = () => {
+  const main = document.querySelector("main");
+  while (main.firstChild) {
+    main.removeChild(main.firstChild);
   }
 }
+class Player {
+  constructor(name, roundIndx, bracketIndx, playerIndx, status) {
+    this.name = name;
+    this.roundIndx = roundIndx;
+    this.bracketIndx = bracketIndx;
+    this.playerIndx = playerIndx;
+    this.status = status;
+  }
+  getBtn() {
+    if (this.status === 'Idle') {
+      return `<button class="p-3 bg-slate-500" data-roundIndex=${this.roundIndx}  data-bracketIndex=${this.bracketIndx}  data-playerIndex=${this.playerIndx}>${this.name}</button>`
+    } else if (this.status === 'Winner') {
+      return `<button class="p-3 bg-emerald-500" data-roundIndex=${this.roundIndx}  data-bracketIndex=${this.bracketIndx}  data-playerIndex=${this.playerIndx}>${this.name}</button>`
+    } else {
+      return `<button class="p-3 bg-red-500 line-through" data-roundIndex=${this.roundIndx}  data-bracketIndex=${this.bracketIndx}  data-playerIndex=${this.playerIndx}>${this.name}</button>`
+    }
+  }
+  setStatus(status) {
+    this.status = status;
+  }
+}
+run();
